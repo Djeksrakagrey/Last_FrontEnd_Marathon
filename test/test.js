f7271901f5a98c6f235de546d6021bd45c35484c
Djeksrakagrey <egory4ev@mail.ru>
2022-02-01 00:10:21 +0800

    add function showVerticalMessage

--- test/test.js
+++ test/test.js
@@ -1,5 +1,66 @@
-let i = 0;
-while ( i < 3) {
-    console.log( `number ${i}!` );
-    i++;
+
+function showVerticalMessage(str) {
+    let changeStr = '';
+    if (str[0] === 'м') {
+        str = 'М' + str.slice(1);
+    }
+
+    if (str.length > 10) {
+        changeStr = str.slice(0, 10);
+    } else {
+         changeStr = str;
+    }
+
+    for (let char of changeStr) {
+        console.log(char);
+    }
 }
+
+showVerticalMessage('марафон');
+
+
+
+
+
+
+
+
+// function truncate(str, maxlength) {
+//
+//     if (str.length >= maxlength) {
+//         return str.slice(0, maxlength - 1) + '...' ;
+//     } else {
+//         return str;
+//     }
+//
+// }
+//
+// console.log(truncate('Hello World', 12));
+
+// // function chekSpam(str) {
+// //
+// //     let lowerStr = str.toLowerCase()
+// //
+// //     if (lowerStr.includes('viagra') ||
+// //         lowerStr.includes('xxx')) {
+// //         return true;
+// //     } else {
+// //         return false;
+// //     }
+// // }
+//
+// console.log(chekSpam('buy viAgRa now'));
+// console.log(chekSpam('free xXXxx'));
+// console.log(chekSpam('innocent rabbit'));
+
+// function ucFirst(str) {
+//     return str[0].toUpperCase() + str.slice(1);
+// }
+//
+// console.log(ucFirst('vova') == 'Vova');
+
+// let i = 0;
+// while ( i < 3) {
+//     console.log( `number ${i}!` );
+//     i++;
+// }
