96699b8077d769c8469f89ddb8e03c0a1604101a
Djeksrakagrey <egory4ev@mail.ru>
2022-02-17 21:37:21 +0800

    add calculator with UI.

--- Calculator_with_UI/main.js
+++ Calculator_with_UI/main.js
@@ -1,7 +1,66 @@
-function log() {
-    for (let elem of document.body.children) {
-    console.log(elem);
+
+const numbers = document.querySelectorAll('.operants');
+const operators = document.querySelectorAll('.operators');
+const screen = document.getElementById('result');
+const dropping = document.getElementById('dropping');
+const backspace = document.getElementById('backspace');
+const total = document.getElementById('total');
+
+function droppingScreen() {
+    screen.textContent = '0';
+}
+dropping.addEventListener('click', droppingScreen);
+
+function backspaceScreen() {
+    const screenLength = screen.textContent.length;
+    if (screenLength == 1) {
+        screen.textContent = '0';
+    } else {
+        screen.textContent = screen.textContent.slice(0, screenLength - 1);
+    }
+}
+backspace.addEventListener('click', backspaceScreen);
+
+
+for (let number of numbers) {
+    number.addEventListener('click', function () {
+        if (screen.textContent != 0) {
+            screen.textContent += number.textContent;
+        }
+        else {
+            screen.textContent = number.textContent;
+        }
+
+    })
+};
+
+
+function calc (operator, a, b) {
+    const CheckValid = (operator === undefined) || (a === undefined) || (b === undefined) || (typeof a !== 'number') ||
+        (typeof b !== 'number');
+
+    if (CheckValid) {
+        return 'ERROR'
+    } else {
+        switch (operator) {
+            case 'sum':
+                return a + b;
+            case 'min':
+                return a - b;
+            case 'multi':
+                return a * b;
+            case 'div':
+                return a / b;
+            case 'remOfDiv':
+                return a % b;
+            case 'exp':
+                return a ** b;
+            default:
+                return 'unknown operation';
+        }
     }
 }
 
-log();
+
+
+
